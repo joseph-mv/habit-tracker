2457ff38437311c61b6809169b149aaac69a9c2c
joseph-mv <josephmv43@gmail.com>
2025-07-22 06:30:34 +0530

    feat: integrate redux-persist for state persistence
    - Configured redux-persist with AsyncStorage
    - Persisted 'habits' slice to retain data across sessions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
-import { store } from "@/src/store";
+import { persistor, store } from "@/src/store";
 import { Stack } from "expo-router";
+import { ActivityIndicator } from "react-native";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function RootLayout() {
-  return <Provider store={store}><Stack /></Provider> ;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<ActivityIndicator/>} persistor={persistor} >
+      <Stack />
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
-import habitReducer from './reducers/habitSlice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import habitReducer from "./reducers/habitSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const rootReducer = combineReducers({
+  habits: habitReducer,
+  // add more reducers here
+});
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["habits"], // reducers to persist
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    habits:habitReducer
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // redux-persist related actions to ignore :These are not normal actions — they include internal logic/data that’s non-serializable.
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 // Types for entire state & dispatch
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
